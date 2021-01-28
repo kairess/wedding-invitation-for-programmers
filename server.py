c02416cf2d9089d431a5703583846825a385a4dc
Taehee <kairess87@gmail.com>
2021-01-28 23:56:07 +0900

    cors

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json, os
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 barrage_path = 'data/barrages.json'
 
